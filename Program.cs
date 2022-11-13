edab6ca0db2c083c20abb201fe7ef09f26c6af78
anassuv <114801727+anassuv@users.noreply.github.com>
2022-11-13 09:26:44 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                x *= a;
+            }
+            Console.Write("{0}, {1} -> {2}\n\n", a, b, x);
+            Console.ReadKey();
+        }
+    }
